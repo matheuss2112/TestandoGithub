c133081f5ba107a7deb2f9d10eab5d1c22e816ae
matheuss2112 <matheushenrique2112@outlook.com>
2022-06-27 13:27:37 -0300

    Fazendo alterações teste

--- java/While.java
+++ java/While.java
@@ -2,7 +2,7 @@ public class While {
     public static void main(String[] args) {
         for(int contador = 0 /* variavel a ser trabalhada*/; contador <= 10 /*condição*/; contador++/*o que ira ser executado */){
             System.out.println(contador);
-
+        System.out.println("Olá Mundo!!");
         }
     }
 
